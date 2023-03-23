61fd04423523ba131760678c1f8c97f029289ca7
Glass-ramen <104834480+Glass-ramen@users.noreply.github.com>
2023-03-23 15:43:36 +0000

    SQL injection protection
    Most SQL Keywords will now be scanned for

--- Express_server/Express server/express_server.js
+++ Express_server/Express server/express_server.js
@@ -16,39 +16,21 @@ app.get("/", function(req, res){
 });
 
 
-// When a post request is recieved
-app.post("/submit-data", function(req,res) {
-  //Fetch data from post form request to then pass to database
-  //Will need to query to figure out if it exists or not
-  //Input needs to be sanatised before it is passed to database
-
-  //Strips ' out of the firstname and lastname
-  var first =  req.body.firstName;
-  first = first.replaceAll("'","");
-  first = first.trim();
-
-  var last =  req.body.lastName;
-  last = last.replaceAll("'","");
-  last = last.trim();
-  var name = first + " " + last;
-  
+function sanatise(things){
   var charCount = 0;
-  while(charCount < name.length){
-    if(name.charAt(charCount).toUpperCase() == "S"){
+  while(charCount < things.length){
+    if(things.charAt(charCount).toUpperCase() == "S"){
       charCount++;
-      if(name.charAt(charCount).toUpperCase() == "E"){
+      if(things.charAt(charCount).toUpperCase() == "E"){
         charCount++;
-        if(name.charAt(charCount).toUpperCase() == "L"){
+        if(things.charAt(charCount).toUpperCase() == "L"){
           charCount++;
-          if(name.charAt(charCount).toUpperCase() == "E"){
+          if(things.charAt(charCount).toUpperCase() == "E"){
             charCount++;
-            if(name.charAt(charCount).toUpperCase() == "C"){
+            if(things.charAt(charCount).toUpperCase() == "C"){
               charCount++;
-              if(name.charAt(charCount).toUpperCase() == "T"){
-                res.send("NO");
-                //Do something to deny the request and get them to retry
-                //Should probably send them back to Index.html but with a warning added to the body stating that those characters are not allowed
-                //Send something to client side to then let JQuery script handle to change html
+              if(things.charAt(charCount).toUpperCase() == "T"){
+                return false;
               }
             }
           }
@@ -57,9 +39,120 @@ app.post("/submit-data", function(req,res) {
     }
     charCount++;
   }
-  //Once all sanatisation has occured, the database will need to be queried to see if that account already exists
+  charCount = 0;
+  while(charCount < things.length){
+    if(things.charAt(charCount).toUpperCase() == "C"){
+      charCount++;
+      if(things.charAt(charCount).toUpperCase() == "R"){
+        charCount++;
+        if(things.charAt(charCount).toUpperCase() == "E"){
+          charCount++;
+          if(things.charAt(charCount).toUpperCase() == "A"){
+            charCount++;
+            if(things.charAt(charCount).toUpperCase() == "T"){
+              charCount++;
+              if(things.charAt(charCount).toUpperCase() == "E"){
+                return false;
+              }
+            }
+          }
+        }
+      }
+    }
+    charCount++;
+  }
+  charCount = 0;
+  while(charCount < things.length){
+    if(things.charAt(charCount).toUpperCase() == "D"){
+      charCount++;
+      if(things.charAt(charCount).toUpperCase() == "R"){
+        charCount++;
+        if(things.charAt(charCount).toUpperCase() == "O"){
+          charCount++;
+          if(things.charAt(charCount).toUpperCase() == "P"){
+            return false;
+          }
+        }
+      }
+    }
+    charCount++;
+  }
+  charCount = 0;
+  while(charCount < things.length){
+    if(things.charAt(charCount).toUpperCase() == "T"){
+      charCount++;
+      if(things.charAt(charCount).toUpperCase() == "A"){
+        charCount++;
+        if(things.charAt(charCount).toUpperCase() == "B"){
+          charCount++;
+          if(things.charAt(charCount).toUpperCase() == "L"){
+            charCount++;
+            if(things.charAt(charCount).toUpperCase() == "E"){
+              return false;
+            }
+          }
+        }
+      }
+    }
+    charCount++;
+  }
+  var charCount = 0;
+  while(charCount < things.length){
+    if(things.charAt(charCount).toUpperCase() == "I"){
+      charCount++;
+      if(things.charAt(charCount).toUpperCase() == "N"){
+        charCount++;
+        if(things.charAt(charCount).toUpperCase() == "S"){
+          charCount++;
+          if(things.charAt(charCount).toUpperCase() == "E"){
+            charCount++;
+            if(things.charAt(charCount).toUpperCase() == "R"){
+              charCount++;
+              if(things.charAt(charCount).toUpperCase() == "T"){
+                return false;
+              }
+            }
+          }
+        }
+      }
+    }
+    // Clear out other SQL keywords
+    // DROP, FROM, TABLE, UNION, JOIN, INSERT, INTO, CREATE, SELECT  
+    // DROP, TABLE, SELECT, CREATE, INSERT
+
+    charCount++;
+  }
+  return true;
+}  
+
+
+// When a post request is recieved
+app.post("/submit-data", function(req,res) {
+  //Fetch data from post form request to then pass to database
+  //Will need to query to figure out if it exists or not
+  //Input needs to be sanatised before it is passed to database
+
+  var first =  req.body.firstName;
+  first = first.replaceAll("'","");
+  first = first.trim();
+  var last =  req.body.lastName;
+  last = last.replaceAll("'","");
+  last = last.trim();
+  var name = first + " " + last;
   
-  res.send(name + " Submitted Successfully");
+  
+  if(sanatise(name)){
+    res.send(name + " Submitted Successfully");
+  }
+  else{
+    res.send("NO");
+  }
+  
+  
+  //Once all sanatisation has occured, the database will need to be queried to see if that account already exists
+  //Delay the response so that the timing is consistant (without noticably slowing down the log in process)
+
+  
 });
 
 //This tells the server to listen on the configured port
