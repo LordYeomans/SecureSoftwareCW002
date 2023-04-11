745e9749569db9bbe454a88257c1c142c0c304bc
Glass-ramen <104834480+Glass-ramen@users.noreply.github.com>
2023-04-11 14:21:32 +0100

    Added sessions
    Cleaned up database code and added sessions to test1 allowing the user to log in and out

--- Express_server/Express server/database_code.js
+++ Express_server/Express server/database_code.js
@@ -14,13 +14,11 @@ const client = new Client({ // Makes client object that we connect to
 const createDatabase = async () => {
     try {
         await client.connect();                            // gets connection
-        await client.query('CREATE DATABASE '+databasename, (err) => {
-            if (err){
-                //console.log(err.stack);
-                //console.log("Ignoring the error");
-            }
-        }); // sends queries
-        return true;
+        let exist = await client.query('SELECT datname FROM pg_database WHERE datname = $1',[databasename]);
+        if(!exist.rows[0]['datname'] == databasename){
+            await client.query('CREATE DATABASE $1',[databasename]);
+            //flush
+        }
     } catch (error) {
         console.error(error.stack);
         return true;
@@ -41,7 +39,7 @@ const createTable = async () => {
         })
         await client.connect();
         await client.query('CREATE TABLE users ( user_id serial PRIMARY KEY,username VARCHAR (50) NOT NULL,password VARCHAR (50) NOT NULL,email VARCHAR (255) NOT NULL)');
-        await client.query('CREATE TABLE posts ( post_id serial PRIMARY KEY,user_id serial NOT NULL,post_text VARCHAR (255) NOT NULL,timestamp DATE NOT NULL DEFAULT CURRENT_DATE,FOREIGN KEY (user_id) REFERENCES users (user_id))');
+        await client.query('CREATE TABLE posts ( post_id serial PRIMARY KEY,user_id serial NOT NULL,post_text VARCHAR (255) NOT NULL,timestamp DATE NOT NULL DEFAULT CURRENT_DATE,FOREIGN KEY (user_id) REFERENCES users (user_id))');  
     } catch (error){
         //console.error(error.stack);
         return false;
@@ -54,32 +52,74 @@ const createTable = async () => {
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+var session = require("express-session");
 
 var app = express();
 const port = process.env.PORT || 5000; //This will listen on localhost:5000
-// ----
-
 // Enables text to be from the html
 app.use(bodyParser.urlencoded({extended: false}));
+// ----
+
+
+
+
+
+
+
+
+const oneDay = 24* 60* 60* 1000;
+// Secret should be array of random strings to stop session hijacking (Secure cookies should also be used)
+app.use(session({
+    secret: "Session1",
+    saveUninitialized: true,
+    resave: false,
+    cookie: {maxAge: oneDay}
+}));
+
+
+
+function isAuthenticated (req, res, next) {
+    if (req.session.logedin) next()
+    else next('route')
+  }
+  
+app.get('/', isAuthenticated, function (req, res) {
+    // this is only called when there is an authentication user due to isAuthenticated
+    res.sendFile(path.join(__dirname, "test1pg2.html"));
+})
 
 // When a get request is recieved the html page is returned
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "test1.html"));
 });
+
 /*
 // Imports the Style Sheet
 app.get('/stylesheet.css', (request, responseC) => {
   responseC.sendFile(path.join(__dirname, "stylesheet.css"))
 });
 */
-
+app.post("/logout", express.urlencoded({extended: false}), function(req,res){
+    //Check if they are logged in
+    if(req.session.logedin){
+        //Destroy the session
+        req.session.destroy(function(err){
+            if (err) return next(err);
+            //Send them back to the home page
+            res.redirect("/");
+        });
+    }
+});
 // For registration
-app.post("/submit-data", async function(req,res) {
+app.post("/submit-data", express.urlencoded({ extended: false }), async function(req,res) {
     //get the data from the form
     var validflag = 1;
     const username = req.body.rej_username;
     const email = req.body.rej_email;
     const password = req.body.rej_password;
+
+    //DATA sterilisation goes here (CHECK FOR SPECIAL CHARACTERS)
+
     const emailcheck = [email];
     const values = [username,password,email];
     if (password == ""){
@@ -94,7 +134,7 @@ app.post("/submit-data", async function(req,res) {
         res.send("Need email");
         validflag = 0;
     }
-    if (validflag == 1){
+    if (validflag == 1 && !req.session.logedin){
         try{
             const client = new Client({
                 host: 'localhost',
@@ -107,20 +147,28 @@ app.post("/submit-data", async function(req,res) {
             const tex = 'SELECT email FROM users WHERE email = $1';
             let response = await client.query(tex,emailcheck);
             console.log(response.rows[0]);
-            if(response.rows[0]['email'] == email){
-                res.send("NO");
+            if(response.rows[0] != undefined){
+              res.send("An account with that email already exists");
             }
             else{
-                //console.log("else");
                 // HASHING & SALTING GOES HERE ------------------
                 await client.query('INSERT INTO users(username,password,email) VALUES($1,$2,$3)',values);
-                await res.send("Account created");
+                //await res.send("Account created");
+                req.session.regenerate(function (err){
+                    req.session.logedin = true;
+                    req.session.save(function (err){
+                        if(err) return next(err);
+                        res.redirect('/');
+                    });
+                })
+    
             }
         }catch(error){
             console.error(error);
         }finally{
             await client.end();
         }
+        //could send redirect header or full html page
     }
 });
 
